cf039d28f5c678c1771b57d8125d819fc4b60b24
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-10-14 19:41:17 +0800

    delete invalid controller

--- src/main/java/com/pshs/attendancesystem/controllers/websockets/ScannerController.java
+++
@@ -1,8 +0,0 @@
-package com.pshs.attendancesystem.controllers.websockets;
-
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-
-@EnableWebSocket
-public class ScannerController {
-
-}
