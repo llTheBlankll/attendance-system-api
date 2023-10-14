4f1d91e95d3f5f2080ebd087b29612f29f11984d
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-10-14 10:09:29 +0800

    renamed id and student id

--- src/main/java/com/pshs/attendancesystem/repositories/StudentRepository.java
+++ src/main/java/com/pshs/attendancesystem/repositories/StudentRepository.java
@@ -4,7 +4,7 @@ import com.pshs.attendancesystem.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    Student findStudentById(Long studentLrn);
+    Student findStudentByLrn(Long studentLrn);
 
     boolean existsById(Long id);
     Iterable<Student> findStudentsByStudentGradeLevel_GradeName(String gradeName);
