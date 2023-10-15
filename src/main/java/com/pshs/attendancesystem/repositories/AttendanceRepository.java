e1d8e1cc3bbcce54097e29669a2e116be98c6b92
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-10-15 12:04:40 +0800

    create a method that takes all the attendance data between two dates

--- src/main/java/com/pshs/attendancesystem/repositories/AttendanceRepository.java
+++ src/main/java/com/pshs/attendancesystem/repositories/AttendanceRepository.java
@@ -2,14 +2,10 @@ package com.pshs.attendancesystem.repositories;
 
 import com.pshs.attendancesystem.entities.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import com.pshs.attendancesystem.Enums;
-import java.util.Date;
+
+import java.time.LocalDate;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, Integer> {
-    @Query(value = "SELECT nextval('attendance_id_seq') AS next_series_id;", nativeQuery =
-            true)
-    Integer getNextSeriesId();
-
-    Iterable<Attendance> findByDateAfterAndDateBeforeAndAttendanceStatus(Date startDate, Date endDate, Enums.status status);
+    Iterable<Attendance> findByDateGreaterThanEqualAndDateLessThanEqualAndAttendanceStatus(LocalDate startdate, LocalDate endDate, Enums.status attendanceStatus);
 }
