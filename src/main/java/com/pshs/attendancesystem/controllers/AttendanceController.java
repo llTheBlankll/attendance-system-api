c53b210abd8440f54dcb748bd759ade3a8a801dd
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-10-15 09:00:54 +0800

    add statistics

--- src/main/java/com/pshs/attendancesystem/controllers/AttendanceController.java
+++ src/main/java/com/pshs/attendancesystem/controllers/AttendanceController.java
@@ -8,9 +8,12 @@ import com.pshs.attendancesystem.repositories.StudentRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Time;
+import java.text.ParseException;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.ZoneId;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Optional;
@@ -22,6 +25,8 @@ public class AttendanceController {
 
     private final AttendanceRepository attendanceRepository;
     private final StudentRepository studentRepository;
+    LocalDate today = LocalDate.now();
+    LocalDate firstDayOfWeek = today.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
 
     public AttendanceController(AttendanceRepository attendanceRepository, StudentRepository studentRepository) {
         this.attendanceRepository = attendanceRepository;
@@ -62,9 +67,9 @@ public class AttendanceController {
             attendance.setStudent(student.get());
 
             if (currentLocalTime.isBefore(flagCeremonyTime) && currentLocalTime.isAfter(earliestTimeToArrive)) {
-                attendance.setAttendance_status(Enums.status.ONTIME);
+                attendance.setAttendanceStatus(Enums.status.ONTIME);
             } else if (currentLocalTime.isAfter(flagCeremonyTime)) {
-                attendance.setAttendance_status(Enums.status.LATE);
+                attendance.setAttendanceStatus(Enums.status.LATE);
             } else if (currentLocalTime.isBefore(earliestTimeToArrive)) {
                 // EARLY
             }
@@ -73,7 +78,7 @@ public class AttendanceController {
             attendance.setTime(Time.valueOf(LocalTime.now()));
             attendance.setId(attendanceRepository.getNextSeriesId());
             this.attendanceRepository.save(attendance);
-            return "The student is " + attendance.getAttendance_status();
+            return "The student is " + attendance.getAttendanceStatus();
         }
 
         return "Student does not exist";
@@ -97,5 +102,58 @@ public class AttendanceController {
 
         this.attendanceRepository.save(attendance);
         return "Attendance was updated";
+    }
+
+    @GetMapping("/late/month")
+    public Iterable<Attendance> getLateStudentsThisMonth() throws ParseException{
+        LocalDate firstDayOfMonth = LocalDate.now().withDayOfMonth(1);
+        LocalDate lastDayOfMonth = today.withDayOfMonth(today.lengthOfMonth());
+
+        return this.attendanceRepository.findByDateAfterAndDateBeforeAndAttendanceStatus(
+                Date.from(firstDayOfMonth.atStartOfDay(ZoneId.systemDefault()).toInstant()),
+                Date.from(lastDayOfMonth.atStartOfDay(ZoneId.systemDefault()).toInstant()),
+                Enums.status.LATE
+        );
+    }
+
+    @GetMapping("/ontime/month")
+    public Iterable<Attendance> getOnTimeStudentsThisMonth() throws ParseException {
+
+        LocalDate firstDayOfMonth = LocalDate.now().withDayOfMonth(1);
+        LocalDate lastDayOfMonth = today.withDayOfMonth(today.lengthOfMonth());
+        Date fromFirstMonth = Date.from(firstDayOfMonth.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        Date toLastMonth = Date.from(lastDayOfMonth.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        return this.attendanceRepository.findByDateAfterAndDateBeforeAndAttendanceStatus(
+                fromFirstMonth,
+                toLastMonth,
+                Enums.status.ONTIME
+        );
+    }
+
+    @GetMapping("/late/week")
+    public Iterable<Attendance> getLateStudentsThisWeek() throws ParseException {
+        LocalDate lastDayOfWeek = today.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
+        Date fromFirstWeek = Date.from(firstDayOfWeek.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        Date toLastWeek = Date.from(lastDayOfWeek.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        return this.attendanceRepository.findByDateAfterAndDateBeforeAndAttendanceStatus(
+                fromFirstWeek,
+                toLastWeek,
+                Enums.status.LATE
+        );
+    }
+
+    @GetMapping("/ontime/week")
+    public Iterable<Attendance> getOnTimeStudentsThisWeek() throws ParseException {
+        LocalDate lastDayOfWeek = today.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
+        Date fromFirstWeek = Date.from(firstDayOfWeek.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        Date toLastWeek = Date.from(lastDayOfWeek.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        return this.attendanceRepository.findByDateAfterAndDateBeforeAndAttendanceStatus(
+                fromFirstWeek,
+                toLastWeek,
+                Enums.status.ONTIME
+        );
     }
 }
