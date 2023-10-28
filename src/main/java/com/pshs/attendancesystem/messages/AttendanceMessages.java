f25905e7dc258e690504b1736799f40bdb95c645
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-10-28 14:13:33 +0800

    this will shut up sonarlint warnings

--- src/main/java/com/pshs/attendancesystem/messages/AttendanceMessages.java
+++ src/main/java/com/pshs/attendancesystem/messages/AttendanceMessages.java
@@ -2,7 +2,14 @@ package com.pshs.attendancesystem.messages;
 
 public class AttendanceMessages {
 
+    public static final String ATTENDANCE_LATE = "You are late.";
+
     public static final String ATTENDANCE_NOT_FOUND = "Attendance does not exist.";
     public static final String ATTENDANCE_UPDATED = "Attendance was updated.";
     public static final String ATTENDANCE_DELETED = "Attendance was deleted.";
+    public static final String ATTENDANCE_ONTIME = "You are on time.";
+    public static final String ATTENDANCE_EARLY = "You are early.";
+    private AttendanceMessages() {
+
+    }
 }
