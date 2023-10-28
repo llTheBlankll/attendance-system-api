7457a9ccee7c8b869fb050bc56aec95f21a324d8
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-10-28 15:00:14 +0800

    made back reference to rfid credentials for people not to see it

---
+++ src/main/java/com/pshs/attendancesystem/AttendanceSystemConfiguration.java
@@ -0,0 +1,21 @@
+package com.pshs.attendancesystem;
+
+import java.sql.Time;
+import java.time.LocalTime;
+
+public class AttendanceSystemConfiguration {
+
+    private AttendanceSystemConfiguration() {
+
+    }
+
+    public static class Attendance {
+
+        public static final LocalTime flagCeremonyTime = Time.valueOf("06:30:00").toLocalTime();
+        public static final LocalTime lateTimeArrival = Time.valueOf("07:00:00").toLocalTime();
+        public static final LocalTime onTimeArrival = Time.valueOf("05:00:00").toLocalTime();
+        private Attendance() {
+
+        }
+    }
+}
