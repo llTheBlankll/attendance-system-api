e49ec10c37481499916f9e2080e9707811af9c8f
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-11-01 20:09:43 +0800

    remove spaces, add attendance service and implementation. next is Attendance Statistics

--- src/main/java/com/pshs/attendancesystem/services/StudentService.java
+++ src/main/java/com/pshs/attendancesystem/services/StudentService.java
@@ -3,22 +3,15 @@ package com.pshs.attendancesystem.services;
 import com.pshs.attendancesystem.entities.Student;
 
 public interface StudentService {
-
     String addStudent(Student student);
-
     String deleteStudent(Student student);
-
     String deleteStudentById(Long id);
-
     Iterable<Student> getStudentByGradeLevel(String gradeLevel);
-
     Student getStudentById(Long lrn);
-
     String updateStudent(Student student);
-
     Iterable<Student> getAllStudentWithSectionId(String sectionId);
-
     long countStudentsBySectionId(String sectionId);
-
     Iterable<Student> getAllStudent();
+
+    boolean studentExistsByLrn(Long lrn);
 }
