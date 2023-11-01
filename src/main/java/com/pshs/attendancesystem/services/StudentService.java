40a4bfebabfed4a953884ac4fab493f4cf4d7a52
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-11-01 14:03:32 +0800

    made student service

--- src/main/java/com/pshs/attendancesystem/services/StudentService.java
+++ src/main/java/com/pshs/attendancesystem/services/StudentService.java
@@ -3,12 +3,22 @@ package com.pshs.attendancesystem.services;
 import com.pshs.attendancesystem.entities.Student;
 
 public interface StudentService {
-    Student findStudentByLrn(Long studentLrn);
 
-    boolean existsById(Long id);
+    String addStudent(Student student);
 
-    Iterable<Student> findStudentsByStudentGradeLevel_GradeName(String gradeName);
+    String deleteStudent(Student student);
 
-    boolean existsByStudentGradeLevel_GradeName(String gradeName);
+    String deleteStudentById(Long id);
 
+    Iterable<Student> getStudentByGradeLevel(String gradeLevel);
+
+    Student getStudentById(Long lrn);
+
+    String updateStudent(Student student);
+
+    Iterable<Student> getAllStudentWithSectionId(String sectionId);
+
+    long countStudentsBySectionId(String sectionId);
+
+    Iterable<Student> getAllStudent();
 }
