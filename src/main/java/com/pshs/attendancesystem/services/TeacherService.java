8c3642538d8cded9c23d694153171e31a9816541
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-11-04 19:24:51 +0800

    remove db columns sensitive info  of teachers

--- src/main/java/com/pshs/attendancesystem/services/TeacherService.java
+++ src/main/java/com/pshs/attendancesystem/services/TeacherService.java
@@ -3,17 +3,10 @@ package com.pshs.attendancesystem.services;
 import com.pshs.attendancesystem.entities.Teacher;
 
 public interface TeacherService {
-
     void deleteTeacher(Integer teacherId);
-
     boolean createTeacher(Teacher teacher);
-
     Teacher getTeacher(Integer teacherId);
-
     Iterable<Teacher> getTeacherByLastName(String lastName);
-
-    Iterable<Teacher> getTeacherBySubjectExpertiseName(String subjectName);
     void updateTeacher(Teacher teacher);
-
     Iterable<Teacher> getAllTeachers();
 }
