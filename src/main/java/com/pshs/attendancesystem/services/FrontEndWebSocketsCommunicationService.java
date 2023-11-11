a14be4e4057d11254fce9d0981f6077450a5ff33
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-11-11 23:02:34 +0800

    optimize websocket connection and fix frontend receiving multiple messages at once.

--- src/main/java/com/pshs/attendancesystem/services/FrontEndWebSocketsCommunicationService.java
+++ src/main/java/com/pshs/attendancesystem/services/FrontEndWebSocketsCommunicationService.java
@@ -1,23 +1,37 @@
 package com.pshs.attendancesystem.services;
 
-import com.pshs.attendancesystem.websocket.handlers.FrontEndWebSocketHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class FrontEndWebSocketsCommunicationService {
 
-    private final List<FrontEndWebSocketHandler> handlerList = new ArrayList<>();
+    private final List<WebSocketSession> sessionList = new ArrayList<>();
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public void registerFrontEndHandler(FrontEndWebSocketHandler handler) {
-        handlerList.add(handler);
+    public void registerFrontEndWebSocket(WebSocketSession session) {
+        sessionList.add(session);
     }
 
-    public void sendMessageToFrontEndHandlers(String message) {
-        for (FrontEndWebSocketHandler handler : handlerList) {
-            handler.sendMessageToAllClients(message);
+    public void sendMessageToAllFrontEnd(String message) {
+        logger.info("Sending Front End Servers Message: {}", message);
+        try {
+            for (WebSocketSession session : this.sessionList) {
+                session.sendMessage(new TextMessage(message));
+            }
+        } catch (IOException exception) {
+            logger.error(exception.getMessage());
         }
+    }
+
+    public void removeWebSocketSession(WebSocketSession session) {
+        this.sessionList.remove(session);
     }
 }
