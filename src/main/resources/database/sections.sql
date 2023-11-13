860ecc03bbfecb0e1c35881b542c9628f90c5ce5
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-11-14 05:46:56 +0800

    fix attendance not deleting

---
+++ src/main/resources/database/sections.sql
@@ -0,0 +1,36 @@
+INSERT INTO public.sections (section_id, teacher, room, grade_level, section_name, strand)
+VALUES (260, 5, 402, 12, 'Nick Joaquin', 1);
+INSERT INTO public.sections (section_id, teacher, room, grade_level, section_name, strand)
+VALUES (261, 7, 403, 12, 'Juan Nakpil', 1);
+INSERT INTO public.sections (section_id, teacher, room, grade_level, section_name, strand)
+VALUES (262, 50, 302, 12, 'Marie Curie', 2);
+INSERT INTO public.sections (section_id, teacher, room, grade_level, section_name, strand)
+VALUES (263, 23, 303, 12, 'Fernando Amorsolo', 1);
+INSERT INTO public.sections (section_id, teacher, room, grade_level, section_name, strand)
+VALUES (264, 32, 201, 12, 'Charles Flint', 2);
+INSERT INTO public.sections (section_id, teacher, room, grade_level, section_name, strand)
+VALUES (265, 43, 202, 12, 'Isaac Newton', 2);
+INSERT INTO public.sections (section_id, teacher, room, grade_level, section_name, strand)
+VALUES (266, 34, 203, 12, 'Albert Einstein', 2);
+INSERT INTO public.sections (section_id, teacher, room, grade_level, section_name, strand)
+VALUES (272, 3, 101, 11, 'Casimiro Del Rosario', 2);
+INSERT INTO public.sections (section_id, teacher, room, grade_level, section_name, strand)
+VALUES (273, 11, 103, 11, 'Fe Del Mundo', 2);
+INSERT INTO public.sections (section_id, teacher, room, grade_level, section_name, strand)
+VALUES (274, 15, 201, 11, 'Diosdado Banatao', 2);
+INSERT INTO public.sections (section_id, teacher, room, grade_level, section_name, strand)
+VALUES (275, 31, 202, 11, 'Gregorio Zara', 2);
+INSERT INTO public.sections (section_id, teacher, room, grade_level, section_name, strand)
+VALUES (276, 19, 203, 11, 'Eduardo Quisumbing', 2);
+INSERT INTO public.sections (section_id, teacher, room, grade_level, section_name, strand)
+VALUES (277, 22, 301, 11, 'Claro Lim-Sylianco', 2);
+INSERT INTO public.sections (section_id, teacher, room, grade_level, section_name, strand)
+VALUES (278, 2, 302, 11, 'Jose Rizal', 1);
+INSERT INTO public.sections (section_id, teacher, room, grade_level, section_name, strand)
+VALUES (279, 4, 303, 11, 'Andres Bonifacio', 1);
+INSERT INTO public.sections (section_id, teacher, room, grade_level, section_name, strand)
+VALUES (280, 6, 401, 11, 'Apolinario Mabini', 1);
+INSERT INTO public.sections (section_id, teacher, room, grade_level, section_name, strand)
+VALUES (281, 8, 402, 11, 'Melchora Aquino', 1);
+INSERT INTO public.sections (section_id, teacher, room, grade_level, section_name, strand)
+VALUES (282, 9, 403, 11, 'Gabriela Silang', 1);
