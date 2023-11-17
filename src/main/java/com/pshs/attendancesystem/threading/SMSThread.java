ad5a51071526045850a41365c848c93e36155b16
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-11-17 11:54:30 +0800

    Changed Library to OkHttp

--- src/main/java/com/pshs/attendancesystem/threading/SMSThread.java
+++ src/main/java/com/pshs/attendancesystem/threading/SMSThread.java
@@ -1,19 +1,17 @@
 package com.pshs.attendancesystem.threading;
 
 import com.pshs.attendancesystem.entities.Guardian;
+import com.squareup.okhttp.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.io.IOException;
 
 public class SMSThread extends Thread {
 
     String parentMessage;
     Guardian guardian;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public SMSThread(String parentMessage, Guardian guardian) {
         this.parentMessage = parentMessage;
@@ -22,67 +20,26 @@ public class SMSThread extends Thread {
 
     private void sendSMSMessage() {
         try {
-            // Specify the URL
-            URL url = new URL("http://192.168.1.1/api/sms/send-sms"); // replace with your actual URL
-//            URL url = new URL("http://localhost:8182"); // replace with your actual URL
-
-            // Open a connection to the URL
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-            // Set the request method to POST
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            connection.setRequestProperty("Cookie", "_TESTCOOKIESUPPORT=1; SID=2eb3a6304ad02831d6bd8458a96f6a0f");
-            connection.setRequestProperty("DNT", "1");
-            connection.setRequestProperty("Host", "192.168.1.1");
-            connection.setRequestProperty("Origin", " http://192.168.1.1");
-            connection.setRequestProperty("Referer", "http://192.168.1.1/small/html/smssnew.htm?history=smss.htm");
-            connection.setRequestProperty("User-Agent", " Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36");
-
-            // Enable input/output streams
-            connection.setDoOutput(true);
-
-            // Set request headers
-//            connection.setRequestProperty("Content-Type", "x-www-form-urlencoded");
-
-            // Set LocalDateTime
-            LocalDateTime dateTime = LocalDateTime.now();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            String formattedTime = dateTime.format(formatter);
-
-            // Define the XML data to be sent in the request body
-//            String xmlData = String.format("<?xml version=\"1.0\" encoding=\"UTF-8\" ?><request><Index>-1</Index><Phones><Phone>%s</Phone></Phones><Sca></Sca><Content>%s</Content><Length>%d</Length><Reserved>1</Reserved><Date>%s</Date></request>", message, phoneNumber, message.length(), formattedTime);
-            String xmlData = String.format("<?xml version=\"1.0\" encoding=\"UTF-8\" ?><request><Index>-1</Index><Phones><Phone>%s</Phone></Phones><Sca></Sca><Content>%s</Content><Length>%d</Length><Reserved>1</Reserved><Date>%s</Date></request>", this.guardian.getContactNumber(), this.parentMessage, parentMessage.length(), formattedTime);
-
-            // Get the output stream and write the XML data to it
-            try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
-                outputStream.writeBytes(xmlData);
-                outputStream.flush();
+            OkHttpClient client = new OkHttpClient();
+            MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+            RequestBody body = RequestBody.create(mediaType, "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><request><Index>-1</Index><Phones><Phone>09998216556</Phone></Phones><Sca></Sca><Content>>Ambos Clarrise Jolia G. is on time</Content><Length>6</Length><Reserved>1</Reserved><Date>2023-11-13 23:32:14</Date></request>");
+            Request request = new Request.Builder()
+                .url("http://192.168.1.1/api/sms/send-sms")
+                .method("POST", body)
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .addHeader("Cookie", "_TESTCOOKIESUPPORT=1; SID=2eb3a6304ad02831d6bd8458a96f6a0f")
+                .addHeader("DNT", "1")
+                .addHeader("Host", "192.168.1.1")
+                .addHeader("Origin", " http://192.168.1.1")
+                .addHeader("Referer", " http://192.168.1.1/small/html/smssnew.htm?history=smss.htm")
+                .addHeader("User-Agent", " Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36")
+                .build();
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                logger.info("SMS sent successfully.");
             }
-
-            StringBuilder builder = new StringBuilder();
-            String line;
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            while ((line = reader.readLine()) != null) {
-                builder.append(line);
-            }
-
-            reader.close();
-
-//            // Get the response code
-//            int responseCode = connection.getResponseCode();
-//            System.out.println("Response Code: " + responseCode);
-//            System.out.println("Response Received: " + builder.toString());
-//            System.out.println(xmlData);
-
-
-            // Handle the response, if needed
-            // You can use connection.getInputStream() to read the response
-
-            // Close the connection
-            connection.disconnect();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        } catch (IOException | IllegalArgumentException e) {
+            logger.error("Error sending SMS: {}", e.getMessage());
         }
     }
 
