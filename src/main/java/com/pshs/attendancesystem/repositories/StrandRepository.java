5693d9bd5b2898175808bd3c212c421b493149b6
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-11-21 20:20:20 +0800

    add services and implements for strandcontroller

--- src/main/java/com/pshs/attendancesystem/repositories/StrandRepository.java
+++ src/main/java/com/pshs/attendancesystem/repositories/StrandRepository.java
@@ -2,8 +2,12 @@ package com.pshs.attendancesystem.repositories;
 
 import com.pshs.attendancesystem.entities.Strand;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StrandRepository extends JpaRepository<Strand, Integer> {
+	Iterable<Strand> findByStrandNameContainsIgnoreCase(String strandName);
+
+	boolean existsByStrandName(@NonNull String strandName);
 }
--- src/main/java/com/pshs/attendancesystem/services/StrandService.java
+++ src/main/java/com/pshs/attendancesystem/services/StrandService.java
@@ -3,6 +3,13 @@ package com.pshs.attendancesystem.services;
 import com.pshs.attendancesystem.entities.Strand;
 
 public interface StrandService {
-
 	Iterable<Strand> getAllStrand();
+
+	String createStrand(Strand strand);
+
+	String deleteStrand(Strand strand);
+
+	Iterable<Strand> searchStrandByName(String strandName);
+
+	String updateStrand(Strand strand);
 }
