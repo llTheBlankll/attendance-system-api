7bbe5fa0092f46d70c12cbd0e7c43e9a98569e04
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-12-02 10:30:18 +0800

    fix errors

--- src/main/java/com/pshs/attendancesystem/config/APIConfig.java
+++ src/main/java/com/pshs/attendancesystem/config/APIConfig.java
@@ -1,7 +1,6 @@
 package com.pshs.attendancesystem.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalTime;
