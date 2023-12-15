6de6ca1e978e6d016a866d799b5257ff6e997b8b
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-12-15 09:08:27 +0800

    Add additional functionality for Student Service and GradeLevel.

--- src/main/java/com/pshs/attendancesystem/repositories/GradeLevelRepository.java
+++ src/main/java/com/pshs/attendancesystem/repositories/GradeLevelRepository.java
@@ -2,9 +2,12 @@ package com.pshs.attendancesystem.repositories;
 
 import com.pshs.attendancesystem.entities.Gradelevel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GradeLevelRepository extends JpaRepository<Gradelevel, Integer> {
 
+	@Query("select g from Gradelevel g where upper(g.gradeName) like upper(concat('%', ?1, '%'))")
+	Iterable<Gradelevel> searchGradeLevelByName(String gradeName);
 }
