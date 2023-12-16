a589d4f5b641efc6f131bdc79ca715e60ce9e274
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-12-16 20:42:30 +0800

    Remove websocket handlers cors configuration

--- src/main/java/com/pshs/attendancesystem/websocket/WebSocketConfiguration.java
+++ src/main/java/com/pshs/attendancesystem/websocket/WebSocketConfiguration.java
@@ -43,8 +43,8 @@ public class WebSocketConfiguration implements WebSocketConfigurer {
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry handlerRegistry) {
 		handlerRegistry.addHandler(new ScannerWebSocketHandler(rfidService, frontEndCommunicationService, attendanceService, sectionCommunicationService), "/websocket/scanner").setAllowedOrigins("*");
-		handlerRegistry.addHandler(new FrontEndWebSocketHandler(frontEndCommunicationService), "/websocket/frontend").setAllowedOrigins("*");
-		handlerRegistry.addHandler(new SectionWebSocketHandler(attendanceService, sectionService), "/websocket/section").setAllowedOrigins("*");
-		handlerRegistry.addHandler(new StudentWebSocketHandler(studentService), "/websocket/student").setAllowedOrigins("*");
+		handlerRegistry.addHandler(new FrontEndWebSocketHandler(frontEndCommunicationService), "/websocket/frontend");
+		handlerRegistry.addHandler(new SectionWebSocketHandler(attendanceService, sectionService), "/websocket/section");
+		handlerRegistry.addHandler(new StudentWebSocketHandler(studentService), "/websocket/student");
 	}
 }
