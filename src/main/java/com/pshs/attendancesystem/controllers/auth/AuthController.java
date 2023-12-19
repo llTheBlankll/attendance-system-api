fe83b94c152860948e7fd165e9db0d5551d801ba
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-12-19 10:01:09 +0800

    User on login update last_login in the database.

--- src/main/java/com/pshs/attendancesystem/controllers/auth/AuthController.java
+++ src/main/java/com/pshs/attendancesystem/controllers/auth/AuthController.java
@@ -6,6 +6,7 @@ import com.pshs.attendancesystem.dto.RegisterUserDTO;
 import com.pshs.attendancesystem.entities.User;
 import com.pshs.attendancesystem.security.jwt.JwtService;
 import com.pshs.attendancesystem.services.AuthenticationService;
+import com.pshs.attendancesystem.services.UserService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,10 +16,12 @@ public class AuthController {
 
 	private final JwtService jwtService;
 	private final AuthenticationService authenticationService;
+	private final UserService userService;
 
-	public AuthController(JwtService jwtService, AuthenticationService authenticationService) {
+	public AuthController(JwtService jwtService, AuthenticationService authenticationService, UserService userService) {
 		this.jwtService = jwtService;
 		this.authenticationService = authenticationService;
+		this.userService = userService;
 	}
 
 	@PostMapping("/register")
@@ -30,9 +33,14 @@ public class AuthController {
 	public LoginResponse login(@RequestBody LoginUserDTO loginUserDTO) {
 		User authenticatedUser = authenticationService.signIn(loginUserDTO);
 
-		return new LoginResponse(
+		LoginResponse loginResponse = new LoginResponse(
 			jwtService.generateToken(authenticatedUser)
 		);
+
+		// Update User Last Log in
+		userService.updateUserLastLogin(authenticatedUser.getUsername());
+
+		return loginResponse;
 	}
 
 	@GetMapping("/ping")
