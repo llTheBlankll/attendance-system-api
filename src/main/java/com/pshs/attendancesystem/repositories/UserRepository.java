d038d8572ccac487af0180843d7f05cb20123744
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-12-20 21:28:44 +0800

    add Redis configuration and add @Repository annotation to each repository.

--- src/main/java/com/pshs/attendancesystem/repositories/UserRepository.java
+++ src/main/java/com/pshs/attendancesystem/repositories/UserRepository.java
@@ -4,12 +4,14 @@ import com.pshs.attendancesystem.entities.User;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 
 	Optional<User> findByUserName(String username);
--- src/main/java/com/pshs/attendancesystem/repositories/UsersRoleRepository.java
+++ src/main/java/com/pshs/attendancesystem/repositories/UsersRoleRepository.java
@@ -2,6 +2,8 @@ package com.pshs.attendancesystem.repositories;
 
 import com.pshs.attendancesystem.entities.UsersRole;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsersRoleRepository extends CrudRepository<UsersRole, Integer> {
 }
