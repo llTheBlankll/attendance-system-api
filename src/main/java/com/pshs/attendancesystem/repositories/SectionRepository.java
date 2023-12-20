4e2017e0713102645e11c031c903de839af05fca
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-12-20 14:41:41 +0800

    Remove unused function

--- src/main/java/com/pshs/attendancesystem/repositories/SectionRepository.java
+++ src/main/java/com/pshs/attendancesystem/repositories/SectionRepository.java
@@ -11,18 +11,12 @@ import java.util.Optional;
 
 @Repository
 public interface SectionRepository extends JpaRepository<Section, Integer> {
-	@Query("select (count(s) > 0) from Section s where upper(s.teacher.lastName) like upper(concat('%', ?1, '%'))")
-	boolean isTeacherLastNameExist(String lastName);
-
 	Section findBySectionId(Integer sectionId);
 
 	Optional<Section> findBySectionName(String sectionName);
 
 	@Query("select s from Section s where upper(s.sectionName) like upper(concat('%', ?1, '%'))")
 	Iterable<Section> searchSectionName(String sectionName);
-
-	@Query("select s from Section s where upper(s.teacher.lastName) like upper(concat('%', ?1, '%'))")
-	Iterable<Section> searchTeacherLastName(String lastName);
 
 	@Query("select s from Section s where s.teacher = ?1")
 	List<Section> getAllSectionByTeacher(Teacher teacher);
