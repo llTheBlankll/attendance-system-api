f682c2136906a162fe2ccdb21c93cb3fb0756e53
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-12-20 14:42:15 +0800

    BetweenDate renaming to DateRange and remove unused service

--- src/main/java/com/pshs/attendancesystem/services/AttendanceService.java
+++ src/main/java/com/pshs/attendancesystem/services/AttendanceService.java
@@ -4,7 +4,7 @@ import com.pshs.attendancesystem.entities.Attendance;
 import com.pshs.attendancesystem.entities.Gradelevel;
 import com.pshs.attendancesystem.entities.Strand;
 import com.pshs.attendancesystem.entities.Student;
-import com.pshs.attendancesystem.entities.statistics.BetweenDate;
+import com.pshs.attendancesystem.entities.statistics.DateRange;
 import com.pshs.attendancesystem.enums.Status;
 
 import java.time.LocalDate;
@@ -13,41 +13,37 @@ public interface AttendanceService {
 
 	Iterable<Attendance> getAllAttendances();
 
-	Iterable<Attendance> getAllAttendanceBetweenDateWithStatus(BetweenDate dateRange, Status status);
+	Iterable<Attendance> getAllAttendanceBetweenDateWithStatus(DateRange dateRange, Status status);
 
-	Iterable<Attendance> getAllAttendanceBetweenDate(BetweenDate dateRange);
+	Iterable<Attendance> getAllAttendanceBetweenDate(DateRange dateRange);
 
-	Iterable<Attendance> getStudentAttendanceBetweenDateStatus(long studentLrn, BetweenDate dateRange, Status status);
+	Iterable<Attendance> getStudentAttendanceBetweenDateStatus(long studentLrn, DateRange dateRange, Status status);
 
-	Iterable<Attendance> getAttendanceBetweenDate(long studentLrn, BetweenDate dateRange);
+	Iterable<Attendance> getAttendanceBetweenDate(long studentLrn, DateRange dateRange);
 
-	Iterable<Attendance> getAttendanceInSectionId(Integer sectionId);
+	Iterable<Attendance> getAttendanceInSectionByStatusBetweenDate(Integer sectionId, Status attendanceStatus, DateRange dateRange);
 
-	Iterable<Attendance> getAttendanceInSectionByStatusBetweenDate(Integer sectionId, Status attendanceStatus, BetweenDate dateRange);
+	Iterable<Attendance> getStudentAttendanceInSectionBetweenDate(Integer sectionId, DateRange dateRange);
 
-	Iterable<Attendance> getStudentAttendanceInSectionBetweenDate(Integer sectionId, BetweenDate betweenDate);
+	Iterable<Attendance> getAttendanceInSection(Integer sectionId, DateRange dateRange, Status status);
 
-	Iterable<Attendance> getAttendanceInSectionByDate(Integer sectionId, LocalDate date);
+	Iterable<Attendance> getAttendanceInSection(Integer sectionId, DateRange dateRange);
 
-	Iterable<Attendance> getAttendanceInSection(Integer sectionId, BetweenDate dateRange, Status status);
-
-	Iterable<Attendance> getAttendanceInSection(Integer sectionId, BetweenDate dateRange);
-
-	long countAttendanceInSection(Integer sectionId, BetweenDate dateRange, Status status);
+	long countAttendanceInSection(Integer sectionId, DateRange dateRange, Status status);
 
 	long countAttendanceInSectionByStatusAndDate(Integer sectionId, Status attendanceStatus, LocalDate date);
 
 	long countAttendanceBySectionAndDate(Integer sectionId, LocalDate date);
 
-	long getAllCountOfAttendanceBetweenDate(BetweenDate dateRange, Status status);
+	long getAllCountOfAttendanceBetweenDate(DateRange dateRange, Status status);
 
-	long getAllCountOfAttendanceBetweenDate(long studentLrn, BetweenDate dateRange, Status status);
+	long getAllCountOfAttendanceBetweenDate(long studentLrn, DateRange dateRange, Status status);
 
-	long countAttendanceInSectionByStatusAndBetweenDate(Integer sectionId, Status attendanceStatus, BetweenDate dateRange);
+	long countAttendanceInSectionByStatusAndBetweenDate(Integer sectionId, Status attendanceStatus, DateRange dateRange);
 
-	long countAttendanceBetweenDate(BetweenDate dateRange);
+	long countAttendanceBetweenDate(DateRange dateRange);
 
-	long countStudentAttendanceBetweenDate(Long studentLrn, BetweenDate dateRange);
+	long countStudentAttendanceBetweenDate(Long studentLrn, DateRange dateRange);
 
 	long countByStudentStrandAndDate(Strand strand, LocalDate date, Status status);
 
