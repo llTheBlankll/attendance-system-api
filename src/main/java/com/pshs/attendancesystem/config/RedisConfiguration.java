a5a53cebe289072f2967713e5478dc18e62397f3
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-12-21 11:35:38 +0800

    Add time to live caching and add @Caching when updating user last logged in time

--- src/main/java/com/pshs/attendancesystem/config/RedisConfiguration.java
+++ src/main/java/com/pshs/attendancesystem/config/RedisConfiguration.java
@@ -3,11 +3,14 @@ package com.pshs.attendancesystem.config;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
@@ -34,6 +37,11 @@ public class RedisConfiguration {
 
 	@Bean
 	public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-		return RedisCacheManager.create(redisConnectionFactory);
+		RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+			.entryTtl(Duration.ofMinutes(5));
+
+		return RedisCacheManager.builder(redisConnectionFactory)
+			.cacheDefaults(redisCacheConfiguration)
+			.build();
 	}
 }
