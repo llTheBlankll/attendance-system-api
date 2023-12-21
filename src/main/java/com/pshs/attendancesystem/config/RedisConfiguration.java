ba7223ca69abb57bf89b81aadb926893b059f175
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-12-21 11:30:01 +0800

    Remove Caffeine caching and use Redis Caching in RedisConfiguration

---
+++ src/main/java/com/pshs/attendancesystem/config/RedisConfiguration.java
@@ -0,0 +1,39 @@
+package com.pshs.attendancesystem.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+@EnableCaching
+public class RedisConfiguration {
+
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+		redisStandaloneConfiguration.setHostName("localhost");
+		redisStandaloneConfiguration.setDatabase(0);
+		redisStandaloneConfiguration.setPort(6379);
+		redisStandaloneConfiguration.setPassword("");
+		return new JedisConnectionFactory(redisStandaloneConfiguration);
+	}
+
+	@Bean
+	public RedisTemplate<?, ?> redisTemplate() {
+		RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		redisTemplate.setEnableTransactionSupport(true);
+		redisTemplate.afterPropertiesSet();
+		return redisTemplate;
+	}
+
+	@Bean
+	public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+		return RedisCacheManager.create(redisConnectionFactory);
+	}
+}
