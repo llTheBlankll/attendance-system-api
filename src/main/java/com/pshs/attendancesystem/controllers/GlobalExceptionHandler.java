63fbcd9993311d2176425f85d11dd25694d99fd7
Vince Angelo Batecan <llTheBlankll@gmail.com>
2023-12-21 11:36:46 +0800

    Add handler for no resource found (404 http code) and handler for expired jwt token.

--- src/main/java/com/pshs/attendancesystem/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/pshs/attendancesystem/controllers/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.pshs.attendancesystem.controllers;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 
 @RestControllerAdvice
@@ -12,4 +14,16 @@ public class GlobalExceptionHandler {
 	public String handleAccessDeniedException(AccessDeniedException e) {
 		return e.getMessage();
 	}
+
+	@ExceptionHandler(NoResourceFoundException.class)
+	public String handleNoResourceFoundException(NoResourceFoundException e) {
+		return e.getMessage();
+	}
+
+	@ExceptionHandler(ExpiredJwtException.class)
+	public String handleExpiredJwtException(ExpiredJwtException e) {
+		return "Your token has expired. Please login again.";
+	}
+
+
 }
