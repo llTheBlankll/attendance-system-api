28a1873d5d25e5d16d58ae2c03d08401b12ba64a
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-01-02 21:30:43 +0800

    OpenAPI fix Mix Content of http to https server

--- src/main/java/com/pshs/attendancesystem/config/SpringDocsConfiguration.java
+++ src/main/java/com/pshs/attendancesystem/config/SpringDocsConfiguration.java
@@ -1,9 +1,11 @@
 package com.pshs.attendancesystem.config;
 
 import com.pshs.attendancesystem.impl.ConfigurationService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
@@ -19,6 +21,11 @@ import org.springframework.context.annotation.Configuration;
 	bearerFormat = "JWT",
 	scheme = "bearer",
 	in = SecuritySchemeIn.HEADER
+)
+@OpenAPIDefinition(
+	servers = {
+		@Server(url = "/", description = "Default Server URL")
+	}
 )
 public class SpringDocsConfiguration {
 
